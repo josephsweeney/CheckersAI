b1ce2531f5e3afb96293b86c1fc540baab621f0d
Joe Sweeney <joe@joesweeney.org>
2017-04-02 23:06:59 -0400

    Modify Move interface so its useful

--- src/Move.java
+++ src/Move.java
@@ -1,4 +1,10 @@
 
 public interface Move {
+  // Starting and ending Positions
+  public int src, dest;
+  // Returns a list of captured positions
+  public int[] captures();
+  // Returns the string representation of a move
+  public String toString();
 
 }
