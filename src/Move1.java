43dd1240aacb8dabadf5a3dd31ece0b81be6db3c
Sailesh <saileshsimhadri@gmail.com>
2017-04-05 03:11:59 -0400

    Fix jump moves in GameState1

--- src/Move1.java
+++ src/Move1.java
@@ -1,22 +1,70 @@
+import java.util.ArrayList;
 
-public class Move {
+public class Move1 implements Move{
 	//(x,y) current position, (x2,y2) position move to
-	int _x;
-	int _y;
-	int _x2;
-	int _y2;
-	boolean _b;
+    ArrayList<String> moves;
 
-	public Move(int x, int y, int x2, int y2, boolean b){
-		_x = x;
-		_y = y;
-		_x2 = x2;
-		_y2 = y2;
-		_b = b;
-
+	public Move1(ArrayList<String> moves){
+        this.moves = moves;
 	}
 
-	public void printMove(){
-			System.out.println("("+this._x+","+this._y+"):("+this._x2+","+this._y2+")" );
+    private int parseX(String move){
+        return Integer.parseInt(move.split(":")[0]);
+    }
+    private int parseY(String move){
+        return Integer.parseInt(move.split(":")[1]);
+    }
+
+    public int[] captures(){
+        return null;
+    }
+
+    public ArrayList<String> kills(){
+        ArrayList<String> captures = new ArrayList<String>();
+        for(int i = 0; i<moves.size()-1; i++){
+            if(Math.abs((parseX(moves.get(i)) - parseX(moves.get(i+1)))) == 2 || Math.abs((parseY(moves.get(i)) - parseY(moves.get(i+1)))) == 2){
+                captures.add(averageX(moves.get(i), moves.get(i+1)) +","+averageY(moves.get(i), moves.get(i+1)));
+            }
+        }
+        return captures;
+    }
+
+    private int averageX(String a, String b){
+        return (parseX(a) + parseX(b))/2;
+    }
+    private int averageY(String a, String b){
+        return (parseY(a) + parseY(b))/2;
+    }
+
+    public int destination(){
+        return 0;
+    }
+
+    public int source(){
+        return 0;
+    }
+
+    public int sourceX(){
+        return parseX(moves.get(0));
+    }
+
+    public int sourceY(){
+        return parseY(moves.get(0));
+    }
+
+    public int destX(){
+        return parseX(moves.get(moves.size() -1));
+    }
+    public int destY(){
+        return parseY(moves.get(moves.size() -1));
+    }
+
+	public String toString(){
+        String ret = "";
+        for(int i = 0; i < moves.size() -1; i++){
+           ret += "(" + moves.get(i) + "):";
+        }
+       ret += "(" +moves.get(moves.size() -1) + ")";
+      return ret;
 	}
 }
