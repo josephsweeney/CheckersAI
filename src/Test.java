c46033c7b23272ce6388aba3ae960eb107780702
Sailesh <saileshsimhadri@gmail.com>
2017-04-05 11:32:24 -0400

    Working CheckerGameState1 and tests

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Test{
 
@@ -39,7 +41,17 @@ public class Test{
                        "-", "w", "-", "-", "-", "w", "-", "w",
                        "w", "-", "w", "-", "w", "-", "w", "-"};
 
+        String[] b5 = {"-", "-", "-", "-", "-", "-", "-", "-",
+                      "-", "-", "-", "-", "b", "-", "-", "-",
+                       "-", "-", "-", "w", "-", "-", "-", "-",
+                       "-", "-", "-", "-", "-", "-", "-", "-",
+                       "-", "w", "-", "w", "-", "-", "-", "-",
+                       "-", "-", "-", "-", "-", "-", "-", "-",
+                       "-", "w", "-", "w", "-", "-", "-", "-",
+                       "-", "-", "-", "-", "-", "-", "-", "-"};
 
+
+        Random r = new Random();
 
         CheckersGameState s1 = new CheckersGameState3(1, b);
         CheckersGameState s2 = new CheckersGameState3(2, b);
@@ -68,6 +80,25 @@ public class Test{
         printMoves(s7);
         System.out.println("Result of State 7");
         s7.result(s7.actions().get(0)).printState();
+        System.out.println("Playing game");
+        CheckersGameState s8 = new CheckersGameState3(1, b5);
+        printMoves(s8);
+        s8.result(s8.actions().get(0)).printState();
+        CheckersGameState state = new CheckersGameState3(1, b);
+        state.printState();
+        List<Move> moves = state.actions();
+       while(moves.size() > 0){
+         System.out.println("Possible actions: ");
+         for(Move move : moves) {
+           System.out.println(move);
+         }
+
+        Move action = moves.get(r.nextInt(moves.size()));
+        System.out.println("Chosen action: " + action);
+        state = state.result(action);
+        state.printState();
+        moves = state.actions();
+      }
     }
 
     static void printMoves(CheckersGameState s){
